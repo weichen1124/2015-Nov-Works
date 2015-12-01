97259e6c961af8e80f0318db47320e88e1bce615
weichen1124 <weichen_david@yahoo.com>
2015-11-30 23:22:53 -0800

    remove source files

--- iTaxiInterface/src/com/dds/pathfinder/itaxiinterface/wslookup/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://DDSAddressConnectionManagerService", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.dds.pathfinder.itaxiinterface.wslookup;
